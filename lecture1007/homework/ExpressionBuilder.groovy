5b615225e99016e4018a6b6b1388730d774c7215
Klara Pernicova <klapernicova@gmail.com>
2024-10-08 12:47:22 +0200

    hw 2

--- lecture1007/homework/ExpressionBuilder.groovy
+++ lecture1007/homework/ExpressionBuilder.groovy
@@ -7,14 +7,89 @@
 // Change or add to the code in the script. Reuse the infrastructure code at the bottom of the script.
 class NumericExpressionBuilder extends BuilderSupport {
 
+    private Item root
+
+    public Item rootItem() {
+        return root
+    }
+
+    protected void setParent(Object parent, Object child) {
+        parent.children << child
+    }
+
+    protected Object createNode(Object nodeName) {
+        createNode nodeName, null, null
+    }
+
+    protected Object createNode(Object nodeName, Object value) {
+        createNode nodeName, null, value
+    }
+
+    protected Object createNode(Object nodeName, Map attrs) {
+        createNode nodeName, attrs, null
+    }
+
+    protected Object createNode(Object nodeName, Map attrs, Object value) {
+        final node = new Item(nodeName)
+        if (value) node.value = value
+        if (attrs && attrs.containsKey('value')) {
+            node.value = attrs.get('value')
+        }
+        if (!root) {
+            root = node
+        }
+        return node
+    }
 }
 
 class Item {
+    final String name
+    final List children = []
+    String value
+
+    public Item(String name) {
+        this.name = name
+        if (name == 'power') {
+            this.name = '^'
+        }
+    }
+
+    public priority() {
+        switch (name) {
+            case '+': return 1
+            case '-': return 1
+            case '*': return 2
+            case '/': return 2
+            case '^': return 3
+            case 'number': return 4
+            case 'variable': return 4
+            default: throw new IllegalArgumentException('Unknown name: ' + name)
+        }
+    }
+
     @Override
     public String toString() {
-        super.toString()
+        if (children.size() == 0) {
+            return value
+        }
+        def result = new StringBuilder()
+        for (int i = 0; i < children.size(); i++) {
+            def child = children[i]
+            if (i > 0) {
+                result.append(" $name ")
+            }
+            if (child.priority() < this.priority()) {
+                result.append('(')
+            }
+            result.append(child.toString())
+            if (child.priority() < this.priority()) {
+                result.append(')')
+            }
+        }
+        return result.toString()
     }
 }
+
 //------------------------- Do not modify beyond this point!
 
 def build(builder, String specification) {
