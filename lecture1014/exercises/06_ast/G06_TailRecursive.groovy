d59e07a6544aef59457d4a33d407bfc78455e3f5
Karlord19 <32455665+Karlord19@users.noreply.github.com>
2024-10-19 13:59:32 +0200

    factorial tail recursive

--- lecture1014/exercises/06_ast/G06_TailRecursive.groovy
+++ lecture1014/exercises/06_ast/G06_TailRecursive.groovy
@@ -1,10 +1,9 @@
-//@groovy.transform.TailRecursive
-def fact(BigDecimal n) {
-    if (n < 2) 1
-    else n * fact(n - 1)
+@groovy.transform.TailRecursive
+def fact(int number, BigInteger acc = 1) {
+    number < 2 ? acc : fact(number - 1, number * acc)
 }
 
 println fact(5)
 
 //TASK Make the function tail recursive so that it can pass the following line
-//println fact(10000)
+println fact(10000)
